f297fa58cb0a0ad41df406a21d9e8eebd24ecb97
Keyukemi <keyukemi.ku@gmail.com>
2023-04-14 19:41:51 +0100

    cleared temp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Roboto_Slab } from 'next/font/google'
+
+const robotoslab = Roboto_Slab({
+  subsets: ['latin'],
+  variable: "--font-roboto"
+})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <main className={`${robotoslab.variable} font-sans`}> 
+    return <Component {...pageProps} />
+  </main>
+  
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
